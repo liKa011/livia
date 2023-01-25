9e70d151ee03d4996d4e72579a68e9de3818de35
liKa011 <likarolineers@gmail.com>
2023-01-24 21:30:58 -0300

    DIA1

---
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarefas.Web.Controllers 
+{
+   public class TarefaController : Controller
+   {
+      public IActionResult Create() 
+      {
+         return View();
+      }
+   }
+}
+
